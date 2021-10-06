ee0a999b7c36ccd27fcd33ab43a747a781a27a98
JulieIb03 <91293848+JulieIb03@users.noreply.github.com>
2021-10-05 22:26:14 -0500

    :D

---
+++ Saborit Express/Navbar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home | Saborit Express</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link rel="stylesheet" href="stylenav.css">-->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+            <div class="navbar1">
+                 <ul>
+                    <li><b> For company drivers </b></li>
+                    <li><a href="#">Register</a></li>
+                    <li><a href="#">Log In</a></li>
+                </ul>
+            </div>
+            <nav class="navbar2">
+                <img class="logo" src="https://i.ibb.co/1G4Zj02/Recurso-17.png" alt="logo">
+                    <label for="btn" class="btn-menu">
+                        <span class="material-icons">menu</span>
+                    </label>
+                    <input class="checkbox" type="checkbox" id="btn">
+                    <ul>
+                        <li>
+                            <label for="btn-1" class="btn-submenu">Shipping +</label>
+                            <a href="#shipping" class="shipping">Shipping +</a>
+                            <div class="point">
+                                <span class="material-icons" style="font-size: x-small;">fiber_manual_record</span>
+                            </div>
+                            <input class="checkbox" type="checkbox" id="btn-1">
+                            <ul class="submenu">
+                                <li><a href="#trucks">Our Trucks</a></li>
+                                <li><a href="#chargo">Types of Chargo</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#tracking">Tracking</a>
+                            <div class="point">
+                                <span class="material-icons" style="font-size: x-small;">fiber_manual_record</span>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#contact-us">Contact Us</a>
+                            <div class="point">
+                                <span class="material-icons" style="font-size: x-small;">fiber_manual_record</span>
+                            </div>
+                        </li>
+                        <li>
+                            <a class="cta" href="#quote"><button>GET A QUOTE</button></a>
+                        </li>
+                    </ul>         
+            </nav>
+    </header>
+</body>
+
+</html>
