11e88ec5378ed4059b3c883af6ac8bd63b50a155
JulieIb03 <91293848+JulieIb03@users.noreply.github.com>
2021-10-13 16:27:52 -0500

    Update Quote.php

--- Quote.php
+++ Quote.php
@@ -63,10 +63,10 @@
         </div>
 
         <script src="Javascript.js"></scrip
-            
+        <?php
+        include "Navbar.php";
+        include "footer.php";
+        ?>    
     </body>
 </html>
-<?php
-include "Navbar.php";
-include "footer.php";
-?>
+
