2b76079730deb6a7779fe4cf959f9a2b322ac3eb
JulieIb03 <91293848+JulieIb03@users.noreply.github.com>
2021-10-14 16:56:10 -0500

    Update login.php

--- login.php
+++ login.php
@@ -18,18 +18,20 @@
         <p class="title">LOG IN</p>
       </div>
       <form action="" method="get" class="form-log">
-        <input class="log-input" type="email" placeholder="Email Address" oninvalid="this.setCustomValidity('Required Field')" required/>
-        <input class="log-input" type="password" placeholder="Password" oninvalid="this.setCustomValidity('Required Field')" required/>
+        <input id="email" class="log-input" type="email" placeholder="Email Address"/>
+        <input id="password" class="log-input" type="password" placeholder="Password" oninvalid="this.setCustomValidity('Required Field')" required/>
         <div class="form-log-end">
           <input type="checkbox" id="btn-rm" />
           <label for="btn-rm">&nbsp;Remember me&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
           <a class="forgot" href="#">Forgot Password?</a>
         </div>
+        <div id="errorlogin"></div>
         <div class="form-log">
-          <input type="submit" value="Log In" class="btn" />
+          <input type="submit" value="Log In" class="btn" onclick='return validateLogIn()' />
         </div>
       </form>
     </div>
+    <script src="appLogIn.js"></script>
   </body>
 </html>
 <?php
