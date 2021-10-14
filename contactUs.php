5a434703326221f3d2734e1c329184c41f20ab7c
JulieIb03 <91293848+JulieIb03@users.noreply.github.com>
2021-10-14 16:56:41 -0500

    Update contactUs.php

--- contactUs.php
+++ contactUs.php
@@ -14,26 +14,27 @@
     <form action="" method="GET" class="form-contact">
         <label class="contact-label" for="contact-name" id="a">Name</label><br>
         
-        <input class="contact-input" type="text" name="contact-name" class="casilla" style="padding-left: 15px ;"><br><br>
+        <input id="name" class="contact-input" type="text" name="contact-name" class="casilla" style="padding-left: 15px ;"><br><br>
         <!-- <div class="circulo">a</div> -->
 
         <label class="contact-label" for="mail">Email</label><br>
         <!-- <div class="circulo"> -->
-        <input class="contact-input" type="email" name="mail" class="casilla" style="padding-left: 15px;"><br><br>
+        <input id="email" class="contact-input" type="email" name="mail" class="casilla" style="padding-left: 15px;"><br><br>
         <!-- </div> -->
         <label class="contact-label" for="subject">Subject</label><br>
         <!-- <div> -->
-        <input  class="contact-input" type="text" name="subject" class="casilla" style="padding-left: 15px;"><br><br>
+        <input id="subject" class="contact-input" type="text" name="subject" class="casilla" style="padding-left: 15px;"><br><br>
         <!-- </div> -->
         
         <label class="contact-label" for="message">Message</label><br>
-        <textarea type="text" name="message" id="mensaje" class="casilla-textarea" style="padding: 15px" cols="30" rows="10"></textarea><br><br>
+        <textarea id="message" type="text" name="message" id="mensaje" class="casilla-textarea" style="padding: 15px" cols="30" rows="10"></textarea><br><br>
 
-
-        <input type="submit" value="Submit" class="btn-contact">
+        <div id="errorcontact"></div>
+        <input type="submit" value="Submit" class="btn-contact" onclick='return validateContactUs()'>
     </form>
 </div>
 </div>
+<script src="appContactUs.js"></script>
 </body>
 </html>
 <?php
